51f27c3acca41ea158e7e62f2f2af9613d430bec
Jessica Wang <jessica.l.wang01@gmail.com>
2022-03-20 10:36:45 -0400

    finishes code to specify character types to include, and accounts for invalid answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 // Assignment code here
 
-// THEN my input should be validated and at least one character type should be selected
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-var determineToInclude = function(answer, char_type, chars){
+// Function to determine if to include specified character type
+function determineToInclude(question, char_type, chars){
   
-  answer = parseInt(answer);
+  answer = parseInt(window.prompt(question));
 
   switch (answer) {
     case 0:
@@ -19,54 +16,65 @@ var determineToInclude = function(answer, char_type, chars){
       return chars = chars + char_type;
       break;
     default:
-      window.alert("You did not pick a valid option. Try again.");
+      window.alert("Invalid option. Please try again.");
+      determineToInclude(question, char_type, chars);
       break;
   }
 }
 
+// Function to determine which character types to include
+var passwordCharacters = function(){
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbers = "0123456789"
+  var special_char = "!@#$%^&*()"
 
+  includeLowercase = "Should the password include lowercase? Enter 1 for YES, 0 for NO.";
+  chars = determineToInclude(includeLowercase, lowercase, "")
+  
+  includeUppercase = "Should the password include uppercase? Enter 1 for YES, 0 for NO.";
+  chars = determineToInclude(includeUppercase, uppercase, chars)
+  
+  includeNumbers = "Should the password include numbers? Enter 1 for YES, 0 for NO.";
+  chars = determineToInclude(includeNumbers, numbers, chars)
+  
+  includeSpecialChar = "Should the password include special characters? Enter 1 for YES, 0 for NO.";
+  chars = determineToInclude(includeSpecialChar, special_char, chars)
+
+  if (chars === ""){
+    window.alert("Please select at least one character type to include.")
+    passwordCharacters();
+  }
+  console.log(chars)
+  return chars
+}
+
+// Function to determine password paramaters and generate password
 var generatePassword = function(){
   
   // Get password length from user
   function passwordLength(){
     inputtedLength = window.prompt("What length should the password be? It should be at least 8 characters and no more than 128 characters.");
     if (inputtedLength < 8 || inputtedLength > 128){
+      window.alert("Invalid length. Please try again.");
       passwordLength();
     }
     return inputtedLength
   }
   passwordLength = passwordLength();
-  console.log(passwordLength)
 
-  // Get info on characters to include
-  var lowercase = "abcdefghijklmnopqrstuvwxyz"
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var numbers = "0123456789"
-  var special_char = "!@#$%^&*()"
-  var password = ""
-
-  includeLowercase = window.prompt("Should the password include lowercase? Enter 1 for YES, 0 for NO.");
-  chars = determineToInclude(includeLowercase, lowercase, "")
+  // Get info on characters to include from user
+  window.alert("Specifiy if password should include lowercase, uppercase, numeric or speical characters. Select at least one character type to include.")
+  chars = passwordCharacters();
   
-  includeUppercase = window.prompt("Should the password include uppercase? Enter 1 for YES, 0 for NO.");
-  chars = determineToInclude(includeUppercase, uppercase, chars)
-
-  includeNumbers = window.prompt("Should the password include numbers? Enter 1 for YES, 0 for NO.");
-  chars = determineToInclude(includeNumbers, numbers, chars)
-
-  includeSpecialChar = window.prompt("Should the password include special characters? Enter 1 for YES, 0 for NO.");
-  chars = determineToInclude(includeSpecialChar, special_char, chars)
-  console.log(chars)
-
   // Generate password given length and characters to include
+  var password = ""
   for (var i = 0; i < passwordLength; i++) {
     password += chars.charAt(Math.floor(Math.random() * chars.length));
-    console.log(password)
   }
   return password
 
 }
-
 
 //Write password to the #password input
 function writePassword() {
