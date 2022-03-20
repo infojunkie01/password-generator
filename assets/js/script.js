9ac3c37545928cbe6f665c52c94f62b3ce711ecf
Jessica Wang <jessica.l.wang01@gmail.com>
2022-03-19 21:21:58 -0400

    changes to code to get input for length of password and code that generates password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 // Assignment code here
 
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
 
 
 // Get references to the #generate element
@@ -19,7 +8,7 @@ var generateBtn = document.querySelector("#generate");
 
 
 var determineToInclude = function(answer, char_type, chars){
-    
+  
   answer = parseInt(answer);
 
   switch (answer) {
@@ -35,21 +24,26 @@ var determineToInclude = function(answer, char_type, chars){
   }
 }
 
+
 var generatePassword = function(){
-  console.log('Hello');
-
-  passwordLength_min = window.prompt("What should the minimum password length be in characters?");
-
-  passwordLength_max = window.prompt("What should the maximum password length be in characters?");
-  if (passwordLength_max < passwordLength_min){
-    passwordLength_max = window.prompt("Maximum length must be equal or greater than minimum length entered. Please re-enter maximum length.");
+  
+  // Get password length from user
+  function passwordLength(){
+    inputtedLength = window.prompt("What length should the password be? It should be at least 8 characters and no more than 128 characters.");
+    if (inputtedLength < 8 || inputtedLength > 128){
+      passwordLength();
+    }
+    return inputtedLength
   }
+  passwordLength = passwordLength();
+  console.log(passwordLength)
 
-  // variables for characters to include
+  // Get info on characters to include
   var lowercase = "abcdefghijklmnopqrstuvwxyz"
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var numbers = "0123456789"
   var special_char = "!@#$%^&*()"
+  var password = ""
 
   includeLowercase = window.prompt("Should the password include lowercase? Enter 1 for YES, 0 for NO.");
   chars = determineToInclude(includeLowercase, lowercase, "")
@@ -62,35 +56,24 @@ var generatePassword = function(){
 
   includeSpecialChar = window.prompt("Should the password include special characters? Enter 1 for YES, 0 for NO.");
   chars = determineToInclude(includeSpecialChar, special_char, chars)
+  console.log(chars)
 
-  
-  // generate pwd
-  getPasswordLength = function(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  passwordLength = getPasswordLength(passwordLength_min, passwordLength_max)
-  console.log(passwordLength)
-
+  // Generate password given length and characters to include
   for (var i = 0; i < passwordLength; i++) {
     password += chars.charAt(Math.floor(Math.random() * chars.length));
     console.log(password)
-
   }
-
-
-
-
+  return password
 
 }
 
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
+//Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
